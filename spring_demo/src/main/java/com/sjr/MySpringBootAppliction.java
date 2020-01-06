2a631a629672f41f6fdf3268c0d2fd4851ef1582
刘浩然 <489514478@qq.com>
2020-01-06 22:47:13 +0800

    20200106 提交 springBoot复习

---
+++ spring_demo/src/main/java/com/sjr/MySpringBootAppliction.java
@@ -0,0 +1,12 @@
+package com.sjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAppliction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootAppliction.class);
+    }
+}
---
+++ spring_demo/src/main/java/com/sjr/controller/quickController.java
@@ -0,0 +1,15 @@
+package com.sjr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class quickController {
+
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "hello springBoot";
+    }
+}
